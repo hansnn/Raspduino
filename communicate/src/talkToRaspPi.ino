bf318c848018f94772605ef818207d3347a98294
hansnn <martinmoeh@gmail.com>
2016-01-23 11:54:28 +0100

    Removed unnecessary code
    Removed some code that is specific to how I have implemented this script (in the "// do something with data" section).

--- communicate/src/talkToRaspPi.ino
+++ communicate/src/talkToRaspPi.ino
@@ -1,40 +1,17 @@
-const int TONEDURATION = 300;
-const int TONEPITCH = 1047;
-
-int piezoPin = 7;
-int ledPin = 8;
-
 int data;
 
 void setup() {
   Serial.begin(9600);
-
-  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
   if (Serial.available()) {
     data = Serial.read();
     
-    if (data == 'z') {
-      playSound();
-    }
-
-    else if (data >= '1' && data <= '9') {
-      blink(data - '0'); // - '0' to convert byte to int
-    }
+    // Do something with data
+    
+    // If you are trying to send integers you have to subtract '0' from data (data = data - '0')
+      // If you want to know why, see http://forum.arduino.cc/index.php?topic=44074.0
   }
-}
-
-void blink(int n) {
-  for (int i = 0; i < n; i++) {
-    digitalWrite(ledPin, HIGH);
-    delay(1000);
-    digitalWrite(ledPin, LOW);
-    delay(500);
-  }
-}
-
-void playSound() {
-  tone(piezoPin, TONEPITCH, TONEDURATION);
+  delay(500); // A small delay to avoid bugs when reading continuously
 }
